fbbab5335c085faf91521da7d082ce6b54e9688b
Steve Downey <sdowney@gmail.com>
2026-01-24 20:44:52 -0500

    Add more build infrastructure and formal tests
    Install and test install.
    Makefile to set up build environment with `uv`
    pyproject.toml hijacked for cxxproject

---
+++ installtest/test.cpp
@@ -0,0 +1,9 @@
+// testinstall/test.cpp                                               -*-C++-*-
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include <beman/expected/expected.hpp>
+
+int main() {
+
+    return 0;
+}
